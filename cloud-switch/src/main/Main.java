3362ff54070160a07c1ffa8dd947c6eb7d06b0aa
Heron Yang <heron.yang.tw@gmail.com>
2016-12-29 08:58:39 +0800

    remove unneed testing code

--- cloud-switch/src/main/Main.java
+++ cloud-switch/src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import main.WebServlet;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
